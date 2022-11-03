a5871d941e90e3056e701e0a971c3977fbc59350
Sebastian Londoño <113002509+sebas-lon@users.noreply.github.com>
2022-11-03 14:30:36 -0500

    Add files via upload

---
+++ Sales Project Queries.sql
@@ -0,0 +1,70 @@
+-- Cleansed DIM_Date Table --
+--Calendar
+SELECT 
+  [DateKey], 
+  [FullDateAlternateKey] AS Date, 
+  [EnglishDayNameOfWeek] AS Day, 
+  [EnglishMonthName] AS Month, 
+  Left([EnglishMonthName], 3) AS MonthShort,  
+  [MonthNumberOfYear] AS MonthNo, 
+  [CalendarQuarter] AS Quarter, 
+  [CalendarYear] AS Year 
+FROM 
+ [AdventureWorksDW2019].[dbo].[DimDate]
+WHERE 
+  CalendarYear >= 2019
+
+  --- Customers
+
+  SELECT 
+  c.customerkey AS CustomerKey, 
+  c.firstname AS [First Name], 
+  c.lastname AS [Last Name], 
+  c.firstname + ' ' + lastname AS [Full Name], 
+  CASE c.gender WHEN 'M' THEN 'Male' WHEN 'F' THEN 'Female' END AS Gender,
+  c.datefirstpurchase AS DateFirstPurchase, 
+  g.city AS [Customer City] -- Joined in Customer City from Geography Table
+FROM 
+  [AdventureWorksDW2019].[dbo].[DimCustomer] as c
+  LEFT JOIN AdventureWorksDW2019.dbo.dimgeography AS g ON g.geographykey = c.geographykey 
+ORDER BY 
+  CustomerKey ASC 
+
+  -- Products
+
+  SELECT 
+  p.[ProductKey], 
+  p.[ProductAlternateKey] AS ProductItemCode, 
+  p.[EnglishProductName] AS [Product Name], 
+  ps.EnglishProductSubcategoryName AS [Sub Category], 
+  pc.EnglishProductCategoryName AS [Product Category], 
+  p.[Color] AS [Product Color], 
+  p.[Size] AS [Product Size], 
+  p.[ProductLine] AS [Product Line], 
+  p.[ModelName] AS [Product Model Name], 
+  p.[EnglishDescription] AS [Product Description], 
+  ISNULL (p.Status, 'Outdated') AS [Product Status] 
+FROM 
+  [AdventureWorksDW2019].[dbo].[DimProduct] as p
+  LEFT JOIN AdventureWorksDW2019.dbo.DimProductSubcategory AS ps ON ps.ProductSubcategoryKey = p.ProductSubcategoryKey 
+  LEFT JOIN AdventureWorksDW2019.dbo.DimProductCategory AS pc ON ps.ProductCategoryKey = pc.ProductCategoryKey 
+order by 
+  p.ProductKey asc
+
+  --Intertnet Sales
+
+  SELECT 
+  [ProductKey], 
+  [OrderDateKey], 
+  [DueDateKey], 
+  [ShipDateKey], 
+  [CustomerKey], 
+  [SalesOrderNumber], 
+  [SalesAmount] 
+FROM 
+  [AdventureWorksDW2019].[dbo].[FactInternetSales]
+WHERE 
+  LEFT (OrderDateKey, 4) >= YEAR(GETDATE()) -3
+ORDER BY
+  OrderDateKey ASC
+
